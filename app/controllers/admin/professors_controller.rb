0bdf43bc8b2a7cfd68814379cfc98be89ddcbe06
Esteban Zignego <esteban.zignego@gmail.com>
2016-04-20 16:49:08 -0300

    fix bug in professors controller

--- app/controllers/admin/professors_controller.rb
+++ app/controllers/admin/professors_controller.rb
@@ -1,17 +1,37 @@
 module Admin
   class ProfessorsController < Admin::ApplicationController
 
-    # private
+    private
 
-    #   def permitted_attributes
-    #     [
-    #       :id, :name, :phone_number, :email,
-    #       location_attributes: location_attributes
-    #     ]
-    #   end
+      def permitted_attributes
+        [
+          :id, 
+          person_attributes: person_attributes,
+          medical_data_attributes: medical_data_attributes
+        ]
+      end
 
-    #   def location_attributes
-    #     %i[id province city zip_code street_name street_number details]
-    #   end
+      def person_attributes
+        [
+          :id, :first_name, :last_name, :dni, :birthdate, :age, 
+          :phone_number, :email, :blood_type, :rh_factor, 
+          :medica_insurance, :medica_insurance_affiliate_number, :medica_insurance_phone_number,
+          :emergency_person, :emergency_person_relationship, :emergency_person_phone_number,
+          location_attributes: location_attributes
+        ]
+      end
+
+      def medical_data_attributes
+        [
+          :id, :has_chronic_desease, :chronic_desease_detail, :take_medication, 
+          :medication_detail, :has_medical_background, :medical_background_detail, 
+          :has_allergy, :allergy_detail, :has_specific_diet, :specific_diet_detail, 
+          :is_vegetarian, :doctor_name, :doctor_phone_number, :urgency_clinic
+        ]
+      end
+
+      def location_attributes
+        %i[id province city zip_code street_name street_number details]
+      end
   end
 end
