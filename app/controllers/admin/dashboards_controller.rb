5d58c135601bc064bc7ae770e372241310a213d5
Esteban Zignego <esteban.zignego@gmail.com>
2016-04-28 19:23:27 -0300

    refactor dashboard

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -6,10 +6,10 @@ module Admin
       current_year = Date.today.year
 
       (2013..current_year).each do |year|
-        delegations = Delegation.where(year: 2016).count
-        partakers = Partaker.where(year: 2016).count
-        delegates = Delegate.where(year: 2016).count
-        authorities = Authority.where(year: 2016).count
+        delegations = Delegation.where(year: year).count
+        partakers = Partaker.where(year: year).count
+        delegates = Delegate.where(year: year).count
+        authorities = Authority.where(year: year).count
         year_statistics = {delegations: delegations, partakers: partakers, delegates: delegates, authorities: authorities, year: year}
         @statistics << year_statistics
       end
