b48aeb7cb16881818278ffaa9284f36c72d297fd
Esteban Zignego <esteban.zignego@gmail.com>
2016-04-28 19:04:53 -0300

    add statistics to dashboard

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -1,5 +1,22 @@
 module Admin
   class DashboardsController < Admin::ApplicationController
 
+  	def index
+      @statistics = []
+      current_year = Date.today.year
+
+      (2013..current_year).each do |year|
+        delegations = Delegation.where(year: 2016).count
+        partakers = Partaker.where(year: 2016).count
+        delegates = Delegate.where(year: 2016).count
+        authorities = Authority.where(year: 2016).count
+        year_statistics = {delegations: delegations, partakers: partakers, delegates: delegates, authorities: authorities, year: year}
+        @statistics << year_statistics
+      end
+      @statistics.reverse!
+
+      super
+    end
+
   end
 end
