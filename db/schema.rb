061afbb397ac54d6129bfd0b8acfd34b84cc1f11
Esteban Zignego <esteban.zignego@gmail.com>
2016-05-23 18:05:15 -0300

    add model country

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160517025822) do
+ActiveRecord::Schema.define(version: 20160523204954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "countries", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.string   "flag_image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "dashboards", force: :cascade do |t|
     t.datetime "created_at", null: false
