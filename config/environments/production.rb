2948c0cc8d454bc4d2aab7632326b925b580e04a
Esteban Zignego <esteban.zignego@gmail.com>
2016-05-26 17:38:04 -0300

    change email configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,12 +80,10 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'mnu-bragado.herokuapp.com',
-    user_name:            'mnubragado@gmail.com',
-    password:             'nadiemechapa',
-    authentication:       'plain'}
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address:              'smtp.gmail.com',
+  #   port:                 587,
+  #   domain:               'mnu-bragado.herokuapp.com',
+  #   authentication:       'plain'}
 end
