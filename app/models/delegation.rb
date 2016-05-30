67bb5e94525f2472d04ecfbd11c1d9fb8f8da23b
Esteban Zignego <esteban.zignego@gmail.com>
2016-05-30 19:39:32 -0300

    refactor show delegation view

--- app/models/delegation.rb
+++ app/models/delegation.rb
@@ -1,16 +1,8 @@
 class Delegation < ActiveRecord::Base
 
   has_many :delegates
-  #belongs_to :sti_delegate, class_name: "Delegate"
-  #belongs_to :ag_delegate1, class_name: "Delegate"
-  #belongs_to :ag_delegate2, class_name: "Delegate"
-  #belongs_to :cs_delegate1, class_name: "Delegate"
-  #belongs_to :cs_delegate2, class_name: "Delegate"
-  #belongs_to :segib_delegate1, class_name: "Delegate"
-  #belongs_to :segib_delegate2, class_name: "Delegate"
   belongs_to :country
 
-  #accepts_nested_attributes_for :sti_delegate, :ag_delegate1, :ag_delegate2, :cs_delegate1, :cs_delegate2, :segib_delegate1, :segib_delegate2
   accepts_nested_attributes_for :delegates
 
   delegate :name, to: :country, prefix: true, allow_nil: true
@@ -20,7 +12,7 @@ class Delegation < ActiveRecord::Base
   end
 
   def representative
-    return self.delegates.first
+    return self.delegates.first.try(:index_name)
   end
 
 end
