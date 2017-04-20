287bb120621736ae83ee1f641a0ef483f82409bd
Esteban Zignego <esteban.zignego@gmail.com>
2017-04-20 13:13:34 -0300

    precompile assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,6 +9,10 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( admin/application.js )
+Rails.application.config.assets.precompile += %w( application.js )
+Rails.application.config.assets.precompile += %w( admin/application.css )
+Rails.application.config.assets.precompile += %w( application.css )
 Rails.application.config.assets.precompile += %w( admin/person/form.js )
 Rails.application.config.assets.precompile += %w( agency.css )
 Rails.application.config.assets.precompile += %w( home.css )
