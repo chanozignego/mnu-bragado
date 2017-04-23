19b01d31717552acc9aeedbb7f93c5013128f34d
Esteban Zignego <esteban.zignego@gmail.com>
2017-04-23 17:31:29 -0300

    change config assets file

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,6 +10,7 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 Rails.application.config.assets.precompile += %w( index.css )
+Rails.application.config.assets.precompile += %w( admin/person/form.js )
 # Rails.application.config.assets.precompile += %w( admin/application.js )
 # Rails.application.config.assets.precompile += %w( application.js )
 # Rails.application.config.assets.precompile += %w( admin/application.css )
