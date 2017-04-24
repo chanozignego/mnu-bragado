771814c17d38e6c49af17c104cedc135fea7f453
Esteban Zignego <esteban.zignego@gmail.com>
2017-04-23 22:50:54 -0300

    fix bug in styles

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,25 +1,19 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require jquery
+//= require jquery/jquery.min
 //= require jquery_ujs
 //= require jquery-ui
-//= require_tree .
+//= require bootstrap
 //= require agency
 //= require cbpAnimatedHeader
 //= require classie
 //= require contact_me
 //= require jqBootstrapValidation
 //= require jquery
-//= require bootstrap
 //= require freelancer
 //= require select2.full
+
+//$.fn.select2.defaults.set("theme", "bootstrap");
+
+//$(function() {
+//  app.startNumericInputOnly();
+//});
