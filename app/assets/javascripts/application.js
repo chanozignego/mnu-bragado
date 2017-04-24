649f27631500b0b5461ae42676089f7100140260
Esteban Zignego <esteban.zignego@gmail.com>
2017-04-23 23:02:29 -0300

    add js

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,20 @@
+//= require jquery
+//= require jquery/jquery.min
+//= require jquery_ujs
+//= require jquery-ui
+//= require bootstrap
+//= require agency
+//= require cbpAnimatedHeader
+//= require classie
+//= require contact_me
+//= require jqBootstrapValidation
+//= require jquery
+//= require freelancer
+//= require select2.full
+//= require jquery-easing
+
+//$.fn.select2.defaults.set("theme", "bootstrap");
+
+//$(function() {
+//  app.startNumericInputOnly();
+//});
