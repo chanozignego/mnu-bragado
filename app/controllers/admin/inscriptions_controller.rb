d2e3040fddc424ad94eb90b556a4110720c7121e
Esteban Zignego <esteban.zignego@gmail.com>
2017-05-04 16:59:20 -0300

    refactor about section

--- app/controllers/admin/inscriptions_controller.rb
+++ app/controllers/admin/inscriptions_controller.rb
@@ -1,5 +1,17 @@
 module Admin
   class InscriptionsController < Admin::ApplicationController
 
+    def index(locals = {})
+      search_term = params[:search].to_s.strip
+      page = Administrate::Page::Collection.new(dashboard, order: order)
+
+      render locals: {
+        resources: resource_collection.order(id: :desc),
+        search_term: search_term,
+        page: page,
+        search: search
+      }.merge(locals)
+    end
+
   end
 end
