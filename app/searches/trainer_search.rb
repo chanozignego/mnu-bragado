8d7f8a3d0c6e07e68102ffb450fade79e29d8903
Esteban Zignego <esteban.zignego@gmail.com>
2018-05-07 16:55:55 -0300

    fix bug in trainer search

--- app/searches/trainer_search.rb
+++ app/searches/trainer_search.rb
@@ -6,7 +6,7 @@ class TrainerSearch < Searchlight::Search
 
   def search_full_name_cont
     opt = options[:full_name_cont]
-    query.joins(:person).where("first_name ILIKE '%#{opt}%' OR last_name ILIKE '%#{opt}%'")
+    query.joins(:user).where("users.first_name ILIKE '%#{opt}%' OR users.last_name ILIKE '%#{opt}%'")
   end
 
 end
