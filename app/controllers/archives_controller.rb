ef10e435289b4c31c7aeb4277efc5d06a251bee5
Esteban Zignego <esteban.zignego@gmail.com>
2020-07-04 20:18:00 -0300

    add little fixes in styles

--- app/controllers/archives_controller.rb
+++ app/controllers/archives_controller.rb
@@ -6,8 +6,8 @@ class ArchivesController < ApplicationController
                 {name: "Asamblea General", section: "ag"},
                 {name: "Sala de Tratados", section: "sti"},
                 {name: "Consejo de Seguridad", section: "cs"},
-                {name: "G20", section: "g20"},
                 {name: "ECOSOC", section: "ecosoc"},
+                {name: "ONU Mujeres", section: "onumujeres"},
                 {name: "Autoridades", section: "authorities"}
               ]
     @folder_name = params[:folder_name]
