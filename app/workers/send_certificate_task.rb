ab2dd92fac618446ad2d747eeb31e4a7af510f9c
Esteban Zignego <esteban.zignego@gmail.com>
2021-07-04 19:57:19 -0300

    WIP: new certificate

--- app/workers/send_certificate_task.rb
+++ app/workers/send_certificate_task.rb
@@ -1,15 +1,15 @@
 class SendCertificateTask
 
   def self.run klass
-    klass.where(certificate: false).each do |person|
-      begin
-        PartakersMailer.certificate_email(person).deliver
-        person.certificate = true
-        person.save!
-      rescue StandardError => e
-        puts "Error in: @{person.id}"
-      end
-    end
+    # klass.where(certificate: false).each do |person|
+      # begin
+        PartakersMailer.certificate_email(Partaker.last).deliver_now
+        # person.certificate = true
+        # person.save!
+      # rescue StandardError => e
+      #   puts "Error in: {{person.id}}"
+      # end
+    #end
   end
 
 
